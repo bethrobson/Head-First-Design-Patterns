4fc35c2061a62ee6f6ee0d6b3175bb615c5794a9
Elisabeth Robson <bethrobson@gmail.com>
2014-06-13 15:26:09 -0700

    removed uneeded import

--- src/headfirst/designpatterns/observer/weather/StatisticsDisplay.java
+++ src/headfirst/designpatterns/observer/weather/StatisticsDisplay.java
@@ -1,7 +1,5 @@
 package headfirst.designpatterns.observer.weather;
 
-//import java.util.*;
-
 public class StatisticsDisplay implements Observer, DisplayElement {
 	private float maxTemp = 0.0f;
 	private float minTemp = 200;
