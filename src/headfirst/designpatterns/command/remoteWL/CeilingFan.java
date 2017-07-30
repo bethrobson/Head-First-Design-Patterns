99c107c2dba2c292ee996cb19ba20792456a4120
Jackson Tong <tongtoan2704@gmail.com>
2017-07-30 15:24:50 +0700

    Fix remoteWL/CeilingFan OFF and LOW speeds are using the same constant

--- src/headfirst/designpatterns/command/remoteWL/CeilingFan.java
+++ src/headfirst/designpatterns/command/remoteWL/CeilingFan.java
@@ -3,10 +3,11 @@ package headfirst.designpatterns.command.remoteWL;
 public class CeilingFan {
 	String location = "";
 	int level;
-	public static final int HIGH = 2;
-	public static final int MEDIUM = 1;
-	public static final int LOW = 0;
- 
+	public static final int HIGH = 3;
+	public static final int MEDIUM = 2;
+	public static final int LOW = 1;
+	public static final int OFF = 0;
+
 	public CeilingFan(String location) {
 		this.location = location;
 	}
@@ -32,7 +33,7 @@ public class CeilingFan {
  
 	public void off() {
 		// turns the ceiling fan off
-		level = 0;
+		level = OFF;
 		System.out.println(location + " ceiling fan is off");
 	}
  
