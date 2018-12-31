14c5792e45941c96a91ee76ec652db7930fcd5b4
dberger <cwr.Derek.Berger@asurion.com>
2018-12-31 15:37:53 -0500

    new test class to play with

--- src/main/java/decorator/starbuzzWithSizes/StarbuzzCoffee.java
+++ src/main/java/decorator/starbuzzWithSizes/StarbuzzCoffee.java
@@ -5,23 +5,35 @@ import decorator.starbuzzWithSizes.Beverage.Size;
 public class StarbuzzCoffee {
  
 	public static void main(String args[]) {
-		Beverage beverage = new Espresso();
-		System.out.println(beverage.getDescription() 
+		Beverage beverage = new DarkRoast();
+		System.out.println("Plain beverage 1: 	 " + beverage.getDescription()
 				+ " $" + String.format("%.2f", beverage.cost()));
  
 		Beverage beverage2 = new DarkRoast();
-		beverage2 = new Mocha(beverage2);
-		beverage2 = new Mocha(beverage2);
-		beverage2 = new Whip(beverage2);
-		System.out.println(beverage2.getDescription() 
-				+ " $" + String.format("%.2f", beverage2.cost()));
- 
-		Beverage beverage3 = new HouseBlend();
+		Mocha mocha = new Mocha(beverage2);
+		Mocha mocha2 = new Mocha(mocha);
+		Whip whip = new Whip(mocha2);
+		// note that getDescription delegates to the wrapped instance
+		System.out.println("Decorated beverage 1:  " + whip.getDescription()
+				+ " $" + String.format("%.2f", whip.cost()));
+
+		Beverage beverage0 = new Espresso();
+		System.out.println("Plain beverage 2: 	 " + beverage0.getDescription()
+				+ "  $" + String.format("%.2f", beverage0.cost()));
+
+
+		Beverage beverage3 = new Espresso();
 		beverage3.setSize(Size.VENTI);
-		beverage3 = new Soy(beverage3);
+		beverage3 = new Cinnamon(beverage3);
 		beverage3 = new Mocha(beverage3);
+		beverage3 = new Mocha(beverage3);
+		beverage3 = new Soy(beverage3);
 		beverage3 = new Whip(beverage3);
-		System.out.println(beverage3.getDescription() 
+		System.out.println("Decorated beverage 2:  " + beverage3.getDescription()
 				+ " $" + String.format("%.2f", beverage3.cost()));
+
+
 	}
+
+
 }
