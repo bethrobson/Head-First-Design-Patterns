93296d1fc1f0b7c67f83b11fb2078dbedff8c1d8
dberger <cwr.Derek.Berger@asurion.com>
2019-02-21 17:12:58 -0500

    start fleshing out client for curry factory

--- src/main/java/curryfactory/CurryKitchen.java
+++ src/main/java/curryfactory/CurryKitchen.java
@@ -2,12 +2,13 @@ package curryfactory;
 
 public abstract class CurryKitchen {
 
+    abstract Curry prepareCurry(String type);
+
 
 
     Curry orderCurry (String item){
-
-
-
+        Curry curry = prepareCurry(item);
+        return curry;
     }
 
 }
