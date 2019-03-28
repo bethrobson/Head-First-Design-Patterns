39d118c0cf4b130e5a50ee12621e8ea663c22aba
dberger <cwr.Derek.Berger@asurion.com>
2019-03-28 09:59:41 -0400

    replace if statements

--- src/main/java/templatemethod/sort/Duck.java
+++ src/main/java/templatemethod/sort/Duck.java
@@ -9,20 +9,14 @@ public class Duck implements Comparable<Duck> {
 		this.weight = weight;
 	}
  
+	@Override
 	public String toString() {
 		return name + " weighs " + weight;
 	}
   
 	public int compareTo(Duck object) {
- 
-		Duck otherDuck = object;
-  
-		if (this.weight < otherDuck.weight) {
-			return -1;
-		} else if (this.weight == otherDuck.weight) {
-			return 0;
-		} else { // this.weight > otherDuck.weight
-			return 1;
-		}
+
+		// this.weight > otherDuck.weight
+		return Integer.compare(this.weight, object.weight);
 	}
 }
