ac12ffef5aee0076b4a1558fe6407b929fc73a2c
Julie Jarzemsky <jjarzem@gmail.com>
2023-09-14 13:52:53 -0600

    Javadoc comment examples + make constants for snowday thresholds

--- src/headfirst/designpatterns/observer/weather/SchoolCancellationDisplay.java
+++ src/headfirst/designpatterns/observer/weather/SchoolCancellationDisplay.java
@@ -1,7 +1,19 @@
 package headfirst.designpatterns.observer.weather;
 
-public class SchoolCancellationDisplay implements DisplayElement, Observer{
+/**
+ * SchoolCancellationDisplay displays a snow day under snowy or cold conditions
+ */
+public class SchoolCancellationDisplay implements DisplayElement, Observer {
 
+    public static final int SNOWDAY_SNOWFALL_MIN = 12;
+    public static final int SNOWDAY_TEMP_MAX = 0;
+
+    /**
+     * This display is an Observer of WeatherData, which holds the snow and temp conditions
+     * that determine if school gets canceled
+     *
+     * @param weatherData
+     */
     SchoolCancellationDisplay(WeatherData weatherData) {
         weatherData.registerObserver(this);
     }
@@ -11,9 +23,17 @@ public class SchoolCancellationDisplay implements DisplayElement, Observer{
         System.out.println("SNOW DAY!");
     }
 
+    /**
+     * Determine whether to display the cancellation
+     *
+     * @param temp
+     * @param humidity
+     * @param pressure
+     * @param snowFallInches
+     */
     @Override
     public void update(float temp, float humidity, float pressure, float snowFallInches) {
-        if (snowFallInches > 12 || temp < 0) {
+        if (snowFallInches > SNOWDAY_SNOWFALL_MIN || temp < SNOWDAY_TEMP_MAX) {
             display();
         }
     }
