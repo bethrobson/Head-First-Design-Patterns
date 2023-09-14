d881ed6f80076d62441802c2dd54ef17b7b2f9cf
Julie Jarzemsky <jjarzem@gmail.com>
2023-09-14 13:51:27 -0600

    Revert "comment examples. + make hard-coded limits constants"
    This reverts commit affbc45714174bc5a9277776470defdff7b9c2b2.

--- src/headfirst/designpatterns/observer/weather/SchoolCancellationDisplay.java
+++ src/headfirst/designpatterns/observer/weather/SchoolCancellationDisplay.java
@@ -1,19 +1,7 @@
 package headfirst.designpatterns.observer.weather;
 
-/**
- * SchoolCancellationDisplay displays a snow day under snowy or cold conditions
- */
-public class SchoolCancellationDisplay implements DisplayElement, Observer {
+public class SchoolCancellationDisplay implements DisplayElement, Observer{
 
-    public static final int SNOWDAY_SNOWFALL_MIN = 12;
-    public static final int SNOWDAY_TEMP_MAX = 0;
-
-    /**
-     * This display is an Observer of WeatherData, which holds the snow and temp conditions
-     * that determine if school gets canceled
-     *
-     * @param weatherData
-     */
     SchoolCancellationDisplay(WeatherData weatherData) {
         weatherData.registerObserver(this);
     }
@@ -23,17 +11,9 @@ public class SchoolCancellationDisplay implements DisplayElement, Observer {
         System.out.println("SNOW DAY!");
     }
 
-    /**
-     * Determine whether to display the cancellation
-     *
-     * @param temp
-     * @param humidity
-     * @param pressure
-     * @param snowFallInches
-     */
     @Override
     public void update(float temp, float humidity, float pressure, float snowFallInches) {
-        if (snowFallInches > SNOWDAY_SNOWFALL_MIN || temp < SNOWDAY_TEMP_MAX) {
+        if (snowFallInches > 12 || temp < 0) {
             display();
         }
     }
